0b2d7552a768e4a412e9e4f28b7bc82b8e3e6b45
Niek <frankenstein795@gmail.com>
2023-11-23 12:02:07 +0100

    Setup FastAPI project

---
+++ back-end/app/routers/general.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    tags=["General"]
+)
+
+
+@router.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@router.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
