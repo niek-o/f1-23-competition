f32d2b8c2a1a7ffcfbdeb624e129478891681116
mart125 <martbraam18@gmail.com>
2023-12-13 14:45:37 +0100

    first table

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -9,14 +9,53 @@
         <title>Leader Board</title>
       </head>
       <body>
-
-        <h1></h1>
-        <p>This is a paragraph.</p>
-
+        <table>
+          <tr>
+            <th>Username</th>
+            <th>Time</th>
+            <th>Track</th>
+            <th>Date</th>
+          </tr>
+          <tr>
+            <td>Mart</td>
+            <td>01:12.573</td>
+            <td>Zandvoort</td>
+            <td>13/12/2023</td>
+          </tr>
+          <tr>
+            <td>Jay</td>
+            <td>01:22.437</td>
+            <td>Spa</td>
+            <td>07/11/2023</td>
+          </tr>
+          <tr>
+            <td>Niek</td>
+            <td>01:37.569</td>
+            <td>Monza</td>
+            <td>10/09/2023</td>
+          </tr>
+        </table>
       </body>
     </html>
 </template>
 
 <style scoped>
-
+  td:nth-child(even){
+    background-color: #130347;
+  }
+  th {
+    text-align: center;
+}
+td {
+    text-align: left;
+}
+table, th, td {
+  width: 500px;
+  border: rgb(112, 112, 112);
+  border-style: solid;
+  border-collapse: collapse;
+}
+table {
+  border-radius: 50%;
+}
 </style>
