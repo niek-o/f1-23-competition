04329684ac64ba1d3a2db3e77a38092e1c3de8e8
mart125 <martbraam18@gmail.com>
2023-12-13 13:22:09 +0100

    no

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Leader Board</title>
+      </head>
+      <body>
+
+        <h1></h1>
+        <p>This is a paragraph.</p>
+
+      </body>
+    </html>
 </template>
+
+<style scoped>
+
+</style>
