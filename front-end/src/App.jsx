194a594dfdc5fd54980a9192eff0bf564722f917
jayklos0908 <jayklos0908@gmail.com>
2024-01-10 12:17:12 +0100

    leaderboard basis

---
+++ front-end/src/App.jsx
@@ -0,0 +1,9 @@
+import YourTableComponent from './components/YourTableComponent'; // Adjust the path
+
+export default function ParentComponent() {
+  return (
+    <div>
+      <YourTableComponent />
+    </div>
+  );
+}
